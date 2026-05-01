fde26912f213fdd307185da817b456147d982283
johnraivenolazo <johnraivenolazo@gmail.com>
2026-05-01 17:02:42 +0800

    fix(cors): set origin to true for better propagation compatibility

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://main.d1qszdffv8wxj8.amplifyapp.com'],
+    origin: true,
     credentials: true,
   });
   app.setGlobalPrefix('api');
